765c0a9e81e344202a61846bc254ec22d8f4bcc1
Carsten König <Carsten.Koenig@hotmail.de>
2020-10-12 08:34:24 +0200

    Einfache Lösung mit String.Replace

--- DictionaryReplacer.fs
+++ DictionaryReplacer.fs
@@ -8,4 +8,21 @@ type Value = string
 type Dictionary = Map<Key, Value>
 
 let replace (dictionary : Dictionary) (input : Input) : Output =
-    failwith "implement me"
+    let sucheStelle (text : string) =
+        let erstesDollar = text.IndexOf '$'
+        if erstesDollar < 0 then None else
+        let naechstesDollar = text.IndexOf('$', erstesDollar + 1)
+        if naechstesDollar < 0 then None else
+        Some
+            {|
+              text = text.Substring(erstesDollar, naechstesDollar - erstesDollar + 1)
+              key = text.Substring(erstesDollar + 1, naechstesDollar - erstesDollar - 1)
+            |}
+    let rec wiederholtesErsetzen text =
+        match sucheStelle text with
+        | None ->
+            text
+        | Some stelle ->
+            let neuerText = text.Replace(stelle.text, dictionary.[stelle.key])
+            wiederholtesErsetzen neuerText
+    wiederholtesErsetzen input
